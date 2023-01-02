b128d478dd434f95e2ac42d70c64646a92d534a7
Paul Olphert <69307367+luney-sp-76@users.noreply.github.com>
2023-01-02 14:04:16 +0000

    add device state to battery level.text

--- powerManager/Controller/ViewController.swift
+++ powerManager/Controller/ViewController.swift
@@ -28,17 +28,17 @@ class ViewController: UIViewController, DeviceManagerDelegate {
         sender.isSelected = true
         //check battery level
         
-        //change battery percentage to current battery percentage
         //check plug state
         //take action based on if the battery is 100% turn plug off. If battery is above the set Level batteryPercentage
         //turn plug off. Otherwise turn plug on and start checking the database for updates and or api for updates
-        
+    
     }
     
     func didUpdateDevice(_ deviceManager: DeviceManager, device: DeviceModel) {
         let id = device.id
         let name = device.name
-        setBatteryLevel.text = device.state
+        //change battery percentage to current battery percentage
+        batteryPercentageLabel.text = device.state
     }
     
     func didFailWithError(error: Error) {
