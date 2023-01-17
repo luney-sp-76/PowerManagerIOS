aedf5efac6e617e1d2ffa945d23827ac73ff8bc8
Paul Olphert <69307367+luney-sp-76@users.noreply.github.com>
2023-01-17 18:07:34 +0000

    more additions to the Data Model

--- powerManager/Model/DeviceData.swift
+++ powerManager/Model/DeviceData.swift
@@ -16,55 +16,91 @@ struct DeviceData: Decodable {
     let last_updated: String
     let context: Context
     
-    }
+}
 
 struct Attributes: Decodable {
     //alternates Level uses unit_of_measurement while State uses Low Power Mode (which needs changed to be underscored)***
     let unit_of_measurement: String?
-        let Low_Power_Mode: Bool?
-        let id: String?
-        let Confidence: String?
-        let auto_update: Bool?
-        let installed_version: String?
-        let latest_version: String?
-        let release_summary: String?
-        let in_progress: Bool?
-        let release_url: String?
-        let skipped_version: String?
-        let title: String?
-        let entity_picture: String?
-        let next_dawn: String?
-        let next_dusk: String?
-        let next_midnight: String?
-        let next_noon: String?
-        let next_rising: String?
-        let next_setting: String?
-        let elevation: Double?
-        let azimuth: Double?
-        let rising: Bool?
-        let radius:Int?
-        let passive: Bool?
-        let persons: Person?
-        let device_class: String?
-        let editable: Bool?
-        let icon: String?
-        let latitude: Double?
-        let longitude: Double?
-        let gps_accuracy: Double?
-        let source: String?
-        let friendly_name: String
-        let supported_features: String?
-        let user_id: String?
-        }
+    let Available: String?
+    let Name: String?
+    let Country: String?
+    let Total: String?
+    let Types: Types?
+    let Locality: String?
+    let Location: [Location]?
+    let Ocean: String?
+    //**Will be problematic as the have spaces and brackets
+    let Available_Important: String?
+    let Available_Opportunistic: String?
+    let Low_Power_Mode: Bool?
+    let Allows_VoIP: Bool?
+    let Carrier_ID: String?
+    let Carrier_Name: String?
+    let ISO_Country_Code: String?
+    let Mobile_Country_Code: String?
+    let Mobile_Network_Code: String?
+    let Hardware_Address: String?
+    let Administrative_Area: String?
+    let Areas_Of_Interest: String?
+    let Inland_Water: String?
+    let iSO_Country_Code: String?
+    
+    
+    //**
+    let id: String?
+    let Confidence: String?
+    let auto_update: Bool?
+    let installed_version: String?
+    let latest_version: String?
+    let release_summary: String?
+    let in_progress: Bool?
+    let release_url: String?
+    let skipped_version: String?
+    let title: String?
+    let entity_picture: String?
+    let next_dawn: String?
+    let next_dusk: String?
+    let next_midnight: String?
+    let next_noon: String?
+    let next_rising: String?
+    let next_setting: String?
+    let elevation: Double?
+    let azimuth: Double?
+    let rising: Bool?
+    let radius:Int?
+    let passive: Bool?
+    let persons: Person?
+    let device_class: String?
+    let editable: Bool?
+    let icon: String?
+    let latitude: Double?
+    let longitude: Double?
+    let gps_accuracy: Double?
+    let source: String?
+    let source_type: String?
+    let friendly_name: String
+    let supported_features: String?
+    let user_id: String?
+}
+
+
 
 struct Context: Decodable {
-        let id: String
-        let parent_id: String?
-        let user_id: String?
+    let id: String
+    let parent_id: String?
+    let user_id: String?
 }
 
 struct Person: Decodable {
     
+}
+
+struct Types: Decodable {
+    let type: String?
+}
+
+struct Location: Decodable {
+    let location: [Double]?
 }
 
 //Battery Level
