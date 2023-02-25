45961dbf70d184faa905de191816561fa8d54725
Paul Olphert <69307367+luney-sp-76@users.noreply.github.com>
2023-02-25 11:35:21 +0000

    Update Constants.swift

--- powerManager/Constants.swift
+++ powerManager/Constants.swift
@@ -16,7 +16,7 @@ struct K {
     static let baseURL = "https://wfebyv7u1xhb8wl7g44evjkl1o7t5554.ui.nabu.casa/api/"
     static let plugStateUrl = "https://wfebyv7u1xhb8wl7g44evjkl1o7t5554.ui.nabu.casa/api/"
     static let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJmNzI5MGM3OTE0NjE0ODhmOGYzZjFjMDU4YjA2YmRhOSIsImlhdCI6MTY3Mjc1OTk4MCwiZXhwIjoxOTg4MTE5OTgwfQ.X9E2pp6XUxjORMAK_mJSsZK5GG6rv4b-3c8X88eX1yQ"
-    static let decrypt = "secretpassword"
+    static let decrypt = "myr3allys3cr3tpassw0rdn00n3cangu3ss?!"
     static let cellIdentifier = "ReusableCell"
     static let celNibName = "DevicesCell"
     static let turnOff = "turn_off"
