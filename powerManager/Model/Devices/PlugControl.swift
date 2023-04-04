d688bf6499ec741d6e3a8eaeb8aa1a049770c0ab
Paul Olphert <69307367+luney-sp-76@users.noreply.github.com>
2023-04-04 19:36:35 +0100

    Updates PlugManager comments

--- powerManager/Model/Devices/PlugControl.swift
+++ powerManager/Model/Devices/PlugControl.swift
@@ -7,64 +7,92 @@
 
 import Foundation
 import FirebaseAuth
+/**
+ A protocol for objects that manage plug devices.
 
+ Objects conforming to this protocol can be notified of updates to plug devices, and can handle errors that occur while managing plug devices.
+ */
 protocol PlugManagerDelegate {
+    /**
+        Notifies the delegate that a device update has occurred.
+
+        - Parameters:
+           - plugControl: The `PlugControl` object that initiated the update.
+        */
     func didUpdateDevice(_ plugControl: PlugControl)
+    /**
+       Notifies the delegate that an error has occurred while managing a device.
+
+       - Parameters:
+          - error: The error that occurred.
+       */
     func didFailWithError(error: Error)
 }
+/**
+ A struct that manages plug devices.
 
+ This struct includes functions to fetch data for a given plug device and send POST requests to turn the device on or off.
+ */
 struct PlugControl {
     
    // let homeAssistantPostUrl = K.baseURL
     var delegate: PlugManagerDelegate?
     var homeAssistantPostUrl: String?
 
- 
+    /**
+        Fetches data for a given plug device.
+
+        - Parameters:
+           - urlEndPoint: The endpoint for the plug device to fetch data for.
+           - device: The name of the plug device to fetch data for.
+        */
     func fetchPlugData(urlEndPoint: String, device: String) {
         let urlString = "\(APIState.shared.url ?? "Error")services/switch/\(urlEndPoint)"
         print(urlString)
         sendRequest(urlString: urlString, device: device)
     }
     
+    /**
+        Sends a POST request to turn a plug device on or off.
+
+        - Parameters:
+           - urlString: The URL to send the POST request to.
+           - device: The name of the plug device to turn on or off.
+        */
     func sendRequest(urlString: String, device: String) {
         let token = APIState.shared.token
         let plug = device
-        //print("POST task 1 started...")
-        //1: Create a URL
+  
+        // Create a URL
         if let url = URL(string: urlString) {
             var urlRequest = URLRequest(url:url)
-           // print("POST task 1 complete")
-           // print("POST task 2 started...")
-            //2: Prepare JSON data
+      
+            // Prepare JSON data
             let json: [String: Any] = ["entity_id" : "\(plug)"]
             let jsonData = try? JSONSerialization.data(withJSONObject: json)
-           //print("POST task 2 complete")
-            //print("POST task 3 started...")
-            //3: Create POST request
+         
+            // Create POST request
             urlRequest.httpMethod = "POST"
             urlRequest.setValue("Bearer \(token ?? "Error")", forHTTPHeaderField: "Authorization")
             urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
-           // print("POST task 3 complete")
-           // print("POST task 4 started...")
-            //4: Insert the JSON into the POST request
+       
+            // Insert the JSON into the POST request
             urlRequest.httpBody = jsonData
-            //print("POST task 4 complete")
-            //print("POST task 5 started...")
+     
             let task = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
                 guard let data = data, error == nil else {
                     print(error?.localizedDescription ?? "No data")
                     self.delegate?.didFailWithError(error: error!)
                     return
                 }
-                //print("POST task 5 complete")
-                //print("POST task 6 started...")
+              
                 let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
                 if let responseJSON = responseJSON as? [String: Any] {
                     self.delegate?.didUpdateDevice(self)
                     print(responseJSON)
                     
                 }
-                //print("POST task 6 complete")
+                
                 self.delegate?.didUpdateDevice(self)
             }
             
