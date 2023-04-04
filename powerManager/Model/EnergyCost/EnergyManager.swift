a835bc413d1eb7f9211a5e55abb684628a09c8d0
Paul Olphert <69307367+luney-sp-76@users.noreply.github.com>
2023-04-04 09:16:02 +0100

    Update EnergyManager Api  call to the Electricity Cost API

--- powerManager/Model/EnergyCost/EnergyManager.swift
+++ powerManager/Model/EnergyCost/EnergyManager.swift
@@ -10,7 +10,21 @@ import FirebaseAuth
 import FirebaseFirestore
 
 struct EnergyManager {
-    // call the energy cost api with the startdate and end date and dno and voltage as set by the user in Setup and in the statisticsviewcontroller
+    /**
+    Calls the energy cost API with the start date, end date, DNO, and voltage as set by the user in the Setup and StatisticsViewController.
+
+    - Parameters:
+
+     - startDate: A Date object, representing the start date of the range for which the energy data should be fetched.
+     - endDate: A Date object, representing the end date of the range for which the energy data should be fetched.
+     
+    - Completion: A closure that takes an optional array of EnergyModel objects as input
+        - Returns: Void.
+     
+    The function first prints the received start and end dates, and initializes a DateFormatter object with the format "dd-MM-yyyy". It then converts the start and end dates to strings using the date formatter. The function proceeds to fetch energy read data from Firestore for the current user's email, and retrieves the DNO and voltage values from the document.
+
+    Next, the function fetches energy data from the API using the fetchEnergyData function, passing the DNO, voltage, start date, and end date as parameters. Upon completion, the closure provided to fetchEnergyData is executed, and the fetched energy models are passed to the completion closure of the updateEnergyData function.
+    */
     func updateEnergyData(startDate: Date, endDate: Date, completion: @escaping ([EnergyModel]?) -> Void) {
         print("The start date recieved by updateEnergyData is \(startDate) and the end date recieved is \(endDate)")
         let dateFormatter = DateFormatter()
@@ -46,7 +60,22 @@ struct EnergyManager {
     }
 
     
-    // The fetchEnergyData method remains unchanged
+    /**
+    Fetches energy data from the API using the specified DNO, voltage, start date, and end date.
+
+    - Parameters:
+
+     - dno: An Int value, representing the Distribution Network Operator code.
+     - voltage: A String value, representing the voltage level (e.g., "LV" for low voltage).
+     - startDate: A String value, representing the start date of the range for which the energy data should be fetched, in the format "dd-MM-yyyy".
+     - endDate: A String value, representing the end date of the range for which the energy data should be fetched, in the format "dd-MM-yyyy".
+     
+    - Completion:
+     - A closure that takes an optional array of EnergyModel objects as input
+        - Returns:  Void.
+     
+    The function constructs the API URL using the provided parameters and sends an HTTP request to fetch energy data. It then checks whether the response contains valid data and no error. If so, it proceeds to parse the JSON response, extracting energy data and converting it into an array of EnergyModel objects. Finally, the function passes the array of EnergyModel objects to the completion closure. If any error occurs during the process, the function calls the completion closure with a nil value.
+    */
     func fetchEnergyData(dno: Int, voltage: String, startDate: String, endDate: String, completion: @escaping ([EnergyModel]?) -> Void) {
         let urlString = "https://odegdcpnma.execute-api.eu-west-2.amazonaws.com/development/prices?dno=\(dno)&voltage=\(voltage)&start=\(startDate)&end=\(endDate)"
     //https://odegdcpnma.execute-api.eu-west-2.amazonaws.com/development/prices?dno=23&voltage=LV&start=14-02-2023&end=15-02-2023
