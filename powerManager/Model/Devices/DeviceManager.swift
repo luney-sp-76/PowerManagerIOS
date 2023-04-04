9011a51fc413dd177f3be0f40a0ea252b9db0721
Paul Olphert <69307367+luney-sp-76@users.noreply.github.com>
2023-04-04 18:43:40 +0100

    Updates DeviceManager Comments

--- powerManager/Model/Devices/DeviceManager.swift
+++ powerManager/Model/Devices/DeviceManager.swift
@@ -28,16 +28,17 @@ struct DeviceManager  {
     var isOff = true
     let dataProvider = DataProvider()
     var delegate: DeviceManagerDelegate?
-
-
-    //call to return the iphone BatteryLevel state (should be an int) used UrlEndPoint Model to form the endpoints
-    // returns a DeviceModel from the ApiCall Model
+    
+    
+    /**
+     Function calls Home Assistant API endpoint to return the iphone BatteryLevel state (should be an int)
+     */
     func fetchDeviceData(deviceName: String) {
         let urlString = "\(APIState.shared.url ?? "Error")states/\(deviceName)"
         //print(urlString)
         callForData(urlString: urlString)
     }
-    //for testing use switch.0x0015bc002f00edf3 as the urlEndPoint
+
     func fetchPlugState(urlEndPoint: String){
         let urlString = "\(APIState.shared.url ?? "Error")states/\(urlEndPoint)"
         callForData(urlString: urlString)
@@ -48,57 +49,57 @@ struct DeviceManager  {
     func callForData(urlString: String) {
         let token = APIState.shared.token
         
-     
-        //print("\(urlString)task 1")
-        //1: Create a URL
+        //Create a URL
         if let url = URL(string: urlString) {
             var urlRequest = URLRequest(url:url)
             
-            //print("task 2")
-            //2: Create a URLSession
+       
+            //Create a URLSession
             urlRequest.httpMethod = "GET"
             urlRequest.setValue("Bearer \(token ?? "Error")", forHTTPHeaderField: "Authorization")
             urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
             
-           //print("task 3 in Device Manager")
-            //3: Give Session a task
+       
+            //Give Session a task
             let task = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
                 if let error = error {
                     self.delegate?.didFailWithError(error: error)
                     return
                 }
                 guard let safeData = data else {return}
+                
+                // use the Device classes switch and sensor to determine the type of device being handled
+                if let device = self.parseJSON(safeData) {
+                    self.delegate?.didUpdateDevice(self, device: device)
+                }
+            }
             
-            // use the Device classes switch and sensor to determine the type of device being handled
-                        if let device = self.parseJSON(safeData) {
-                            self.delegate?.didUpdateDevice(self, device: device)
-                        }
-                    }
-            
-            //4: Start the task
+            // Start the task
             task.resume()
         }
-       
+        
     }
     
+    /**
+     
+      function parses the Data from the HomeAssistant Device response
+     - Parameters:
+        - Data from device endpoint in the JSON format
+     - Returns:
+        - optional DeviceModel
+     */
     func parseJSON(_ deviceData: Data) -> DeviceModel? {
         
         let decoder = JSONDecoder()
         do {
+            
             let decodedData = try decoder.decode(DeviceData.self, from: deviceData)
             let id = decodedData.entity_id
             let state = decodedData.state
             let name = decodedData.attributes.friendlyName
             let lastUpdate = decodedData.last_updated
             let uuid = decodedData.context.id
-//            print("device name: \(id)")
-//            print("friendly name: \(name)")
-//            print("Unique device ID: \(uuid)")
-//            print("current state:\(state)")
-//            print("last updated: \(lastUpdate)")
-//            print("")
        
-            
             let device = DeviceModel(id: id, state: state, name: name, lastUpdate: lastUpdate, uuid: uuid)
             return device
             
@@ -109,48 +110,49 @@ struct DeviceManager  {
         }
     }
     
-   
     
+    /**
+     A mutating function that manages the battery level of a device.
+
+     - Parameters:
+        - device: The device to manage.
+        - lowestBatteryChargeLevel: The lowest battery level at which the device should be turned on.
+        - currentBatteryLevel: The current battery level of the device.
+        - plugName: The name of the plug device to control.
+
+     - Returns: A string indicating whether the device should be turned on or off.
+     */
     mutating func manageBattery(device: DeviceModel, lowestBatteryChargeLevel: Int, currentBatteryLevel: Int, plugName: String)-> String {
+        
         var returnString = K.on
-//        print("The request from batteryMonitor is recieved..")
-//        print("Current Battery Level \(currentBatteryLevel) which is of type...")
-//        print(type(of: currentBatteryLevel))
-//        print("device is the plug \(device.id == plugName)")
-//        print("lowest Charge level \(lowestBatteryChargeLevel)")
-//        print("The plug is On:  \(device.state == K.on)")
-//        print("The plug state is: \(device.state)")
-//        print("The plug Boolean is Off:  \(isOff == false)")
-        
-        
         if currentBatteryLevel == 100 && device.id == plugName && device.state == K.on {
             //attempting post request
             print("attempting to send a post request....")
             plugControl.fetchPlugData(urlEndPoint: K.turnOff, device: plugName)
             //update firebase
             dataProvider.transferData { result in
-                    switch result {
-                    case .success(_):
-                        break
-                    case .failure(let error):
-                        print("error sending device data \(error)")
-                    }
+                switch result {
+                case .success(_):
+                    break
+                case .failure(let error):
+                    print("error sending device data \(error)")
                 }
+            }
             isOff = true
             print("is OFF Now? \(isOff)")
             returnString = K.off
-
+            
         } else if currentBatteryLevel <= lowestBatteryChargeLevel && device.id == plugName && device.state == K.off {
             print("attempting to send a post request....")
             plugControl.fetchPlugData(urlEndPoint: K.turnOn, device: plugName)
             dataProvider.transferData { result in
-                    switch result {
-                    case .success(_):
-                       break
-                    case .failure(let error):
-                        print("error sending device data \(error)")
-                    }
+                switch result {
+                case .success(_):
+                    break
+                case .failure(let error):
+                    print("error sending device data \(error)")
                 }
+            }
             isOff = false
             print("is OFF Now? \(isOff)")
             returnString = K.on
