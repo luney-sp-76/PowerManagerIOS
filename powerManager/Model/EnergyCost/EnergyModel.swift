e4c2379a6cafdbc1b675945ac9305236f96943c3
Paul Olphert <69307367+luney-sp-76@users.noreply.github.com>
2023-04-04 20:22:43 +0100

    Updates to EnergyModel Comments

--- powerManager/Model/EnergyCost/EnergyModel.swift
+++ powerManager/Model/EnergyCost/EnergyModel.swift
@@ -5,12 +5,26 @@
 //  Created by Paul Olphert on 16/02/2023.
 //
 
-//used to manage the Api call to the Imperical College Londons Electricity Cost Api
+/// Model for energy usage data
 class EnergyModel {
+    /// Overall energy usage value
     var overall: Double
+    
+    /// Unix timestamp for energy usage data
     var unixTimestamp: Double
+    
+    /// Timestamp for energy usage data in string format
     var timestamp: String
     
+    /**
+     
+     Initializer for EnergyModel class
+      - Parameters:
+       - overall: Overall energy usage value
+       - unixTimestamp: Unix timestamp for energy usage data
+       - timestamp: Timestamp for energy usage data in string format
+     
+     */
     init(overall: Double, unixTimestamp: Double, timestamp: String) {
         self.overall = overall
         self.unixTimestamp = unixTimestamp
@@ -19,6 +33,16 @@ class EnergyModel {
 }
 
 extension EnergyModel: Equatable {
+    /**
+     
+     Check if two EnergyModel objects are equal
+        - Parameters:
+         - lhs: First EnergyModel object
+         - rhs: Second EnergyModel object
+     
+        - Returns: True if the two EnergyModel objects have equal overall, unixTimestamp and timestamp values, false otherwise
+     
+     */
     static func == (lhs: EnergyModel, rhs: EnergyModel) -> Bool {
         return lhs.overall == rhs.overall &&
         lhs.unixTimestamp == rhs.unixTimestamp &&
